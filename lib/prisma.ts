52e4bf0f6bfbb008aaaca621a6d83b1e824f686c
angelvisyanez <angelviselyanez@gmail.com>
2026-07-04 15:59:08 -0400

    Fix: usar ESM imports en lugar de require() para adapters Prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,7 @@
 import { PrismaClient } from "./generated/prisma/client"
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3"
+import { PrismaPg } from "@prisma/adapter-pg"
+import { Pool } from "pg"
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
@@ -8,30 +11,15 @@ function createPrismaClient(): PrismaClient {
   const url = process.env.DATABASE_URL ?? "file:./prisma/dev.db"
 
   if (url.startsWith("file:")) {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    const { PrismaBetterSqlite3 } = require("@prisma/adapter-better-sqlite3")
     const adapter = new PrismaBetterSqlite3({ url })
     return new PrismaClient({ adapter })
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
-  const { PrismaPg } = require("@prisma/adapter-pg")
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
-  const { Pool } = require("pg")
   const pool = new Pool({ connectionString: url })
   const adapter = new PrismaPg(pool)
   return new PrismaClient({ adapter })
 }
 
-function getPrisma(): PrismaClient {
-  if (!globalForPrisma.prisma) {
-    globalForPrisma.prisma = createPrismaClient()
-  }
-  return globalForPrisma.prisma
-}
+export const prisma = globalForPrisma.prisma ?? createPrismaClient()
 
-export const prisma = new Proxy({} as PrismaClient, {
-  get(_target, prop) {
-    return getPrisma()[prop as keyof PrismaClient]
-  },
-})
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
